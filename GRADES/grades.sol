bfeb0274ad8a81b651e79153e21788fd92cbcaa3
agustinlopez1999 <lopezagustin1999@hotmail.com>
2023-10-30 13:40:03 -0300

    add initial variables

--- GRADES/grades.sol
+++ GRADES/grades.sol
@@ -1,3 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
+contract grades{
+
+    //Professor address
+    address public professor;
+
+    //Professor address is contract deployer adress
+    constructor(){
+        professor = msg.sender;
+    }
+
+    //Relate hash with grades
+    mapping(bytes32 => uint) Grades;
+
+    //Array from students that ask for revision
+    string[] revisions;
+
+    //Events
+    event student_evaluated(bytes32);
+    event ask_revision(string);
+
+}
